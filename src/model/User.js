38595c02674c9b84eeb789358c9ed9fdc200a382
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-11 14:48:06 +0100

    Create User.js

---
+++ src/model/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Invalid email"],
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+
+    wallet: {
+      type: Number,
+      default: 0,
+    },
+
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
