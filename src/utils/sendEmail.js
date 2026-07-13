b080abffccd327b60504974778ed8105ec031b0a
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-13 13:18:24 +0100

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,1 +1,29 @@
+const sgMail = require("@sendgrid/mail");
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendEmail = async ({
+  to,
+  subject,
+  html,
+}) => {
+  try {
+    await sgMail.send({
+      to,
+      from: process.env.EMAIL_FROM,
+      subject,
+      html,
+    });
+
+    return true;
+  } catch (error) {
+    console.error(
+      "SendGrid Error:",
+      error.response?.body || error.message
+    );
+
+    throw new Error("Failed to send email.");
+  }
+};
+
+module.exports = sendEmail;
