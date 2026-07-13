ffc5eec190ac736b09f0ce4799b894b9573ed9c3
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-13 17:15:41 +0100

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProfile } = require("../controllers/userController");
 const auth = require("../middleware/authMiddleware");
 
+const {
+  getProfile,
+  getWalletBalance,
+  refreshWallet,
+} = require("../controllers/userController");
+
+// Get authenticated user's profile
 router.get("/profile", auth, getProfile);
+
+// Get wallet balance
+router.get("/wallet", auth, getWalletBalance);
+
+// Refresh wallet balance
+router.get("/wallet/refresh", auth, refreshWallet);
 
 module.exports = router;
