f1161c67db7df5c8f12a17edeb19a5012d91aab1
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-13 10:23:52 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,69 @@
+// const mongoose = require("mongoose");
+// const validator = require("validator");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     firstName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+
+//     lastName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//       validate: [validator.isEmail, "Invalid email"],
+//     },
+
+//     password: {
+//       type: String,
+//       required: true,
+//       minlength: 6,
+//     },
+
+//     verified: {
+//       type: Boolean,
+//       default: false,
+//     },
+
+//     wallet: {
+//       type: Number,
+//       default: 0,
+//     },
+
+//     role: {
+//       type: String,
+//       default: "user",
+//       enum: ["user", "admin"],
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = mongoose.model("User", userSchema);
+
 const mongoose = require("mongoose");
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: {
+    username: {
       type: String,
       required: true,
+      unique: true,
       trim: true,
-    },
-
-    lastName: {
-      type: String,
-      required: true,
-      trim: true,
+      minlength: 3,
+      maxlength: 30,
     },
 
     email: {
