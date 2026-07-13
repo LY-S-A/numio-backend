24d6ca8c24ce96301a6c59bd67b13485f6c9ac3c
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-13 13:56:23 +0100

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,43 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   register,
+//   login,
+// } = require("../controllers/authController");
+
+// router.post("/register", register);
+
+// router.post("/login", login);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 
 const {
   register,
   login,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/authController");
 
+// Register
 router.post("/register", register);
 
+// Login
 router.post("/login", login);
+
+// Forgot Password
+router.post(
+  "/forgot-password",
+  forgotPassword
+);
+
+// Reset Password
+router.post(
+  "/reset-password/:token",
+  resetPassword
+);
 
 module.exports = router;
