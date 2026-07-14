3d8ce3a911c4c59d6640fcc5b1f2dde2b0a32446
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-14 10:50:44 +0100

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,35 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-module.exports = async (req, res, next) => {
+// module.exports = async (req, res, next) => {
+//   try {
+//     const authHeader = req.headers.authorization;
+
+//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Unauthorized",
+//       });
+//     }
+
+//     const token = authHeader.split(" ")[1];
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     req.user = decoded;
+
+//     next();
+//   } catch (err) {
+//     return res.status(401).json({
+//       success: false,
+//       message: "Invalid token",
+//     });
+//   }
+// };
+
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.protect = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
@@ -15,7 +44,16 @@ module.exports = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded;
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
 
     next();
   } catch (err) {
