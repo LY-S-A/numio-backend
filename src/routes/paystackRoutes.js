4b1987a7976d0d2bbdcea957f90d9ef4be21a213
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-15 16:49:41 +0100

    Update paystackRoutes.js

--- src/routes/paystackRoutes.js
+++ src/routes/paystackRoutes.js
@@ -1,1 +1,18 @@
+const express = require("express");
 
+const router = express.Router();
+
+const {
+  initializePayment,
+  verifyPayment,
+} = require("../controllers/paystackController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+// Initialize Paystack payment
+router.post("/init", protect, initializePayment);
+
+// Paystack redirect after payment
+router.get("/verify", verifyPayment);
+
+module.exports = router;
