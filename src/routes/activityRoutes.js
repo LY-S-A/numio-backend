a5ae8b04ffb12d047b9434b09b90d2300756d7ec
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-25 17:59:37 +0100

    Create activityRoutes.js

---
+++ src/routes/activityRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    getLiveActivities,
+} = require("../controllers/activityController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.get("/live", protect, getLiveActivities);
+
+module.exports = router;
