d99ea9d61357b8392b04f1cd15fcd2292956c106
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-25 20:54:06 +0100

    Update activityRoutes.js

--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const {
     getLiveActivities,
+    getDashboardStats,
 } = require("../controllers/activityController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/live", protect, getLiveActivities);
+
+router.get("/stats", protect, getDashboardStats);
 
 module.exports = router;
