cf644c069b469b487b42063e42872f8bf3c4f8f9
lysa.waitlist <Lysauniverse30@gmail.com>
2026-07-28 10:23:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://numio-one.vercel.app",
-  "https://numioverify.store",
+  "https://www.numioverify.store",
 ];
 
 app.use(
