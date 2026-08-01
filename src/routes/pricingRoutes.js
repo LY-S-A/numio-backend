74138b3bc364d4c30f2a2a0a359119f826a12979
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-01 14:20:32 +0100

    Update pricingRoutes.js

--- src/routes/pricingRoutes.js
+++ src/routes/pricingRoutes.js
@@ -1,46 +1,46 @@
-const express = require("express");
+// const express = require("express");
 
-const router = express.Router();
+// const router = express.Router();
 
-const protect = require("../middleware/auth");
+// const protect = require("../middleware/auth");
 
-const admin = require("../middleware/admin");
+// const admin = require("../middleware/admin");
 
-const pricingController =
-require("../controllers/pricingController");
+// const pricingController =
+// require("../controllers/pricingController");
 
-router.use(protect);
+// router.use(protect);
 
-router.use(admin);
+// router.use(admin);
 
-router.get(
-    "/rules",
-    pricingController.getRules
-);
+// router.get(
+//     "/rules",
+//     pricingController.getRules
+// );
 
-router.post(
-    "/rules",
-    pricingController.createRule
-);
+// router.post(
+//     "/rules",
+//     pricingController.createRule
+// );
 
-router.put(
-    "/rules/:id",
-    pricingController.updateRule
-);
+// router.put(
+//     "/rules/:id",
+//     pricingController.updateRule
+// );
 
-router.delete(
-    "/rules/:id",
-    pricingController.deleteRule
-);
+// router.delete(
+//     "/rules/:id",
+//     pricingController.deleteRule
+// );
 
-router.get(
-    "/exchange-rate",
-    pricingController.getExchangeRate
-);
+// router.get(
+//     "/exchange-rate",
+//     pricingController.getExchangeRate
+// );
 
-router.put(
-    "/exchange-rate",
-    pricingController.updateExchangeRate
-);
+// router.put(
+//     "/exchange-rate",
+//     pricingController.updateExchangeRate
+// );
 
-module.exports = router;
+// module.exports = router;
