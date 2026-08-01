a7f62c4a67fa499a199a198171a73ae7aba4c395
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-01 14:20:08 +0100

    Create  pricingRoutes.js

---
+++ src/routes/pricingRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const router = express.Router();
+
+const protect = require("../middleware/auth");
+
+const admin = require("../middleware/admin");
+
+const pricingController =
+require("../controllers/pricingController");
+
+router.use(protect);
+
+router.use(admin);
+
+router.get(
+    "/rules",
+    pricingController.getRules
+);
+
+router.post(
+    "/rules",
+    pricingController.createRule
+);
+
+router.put(
+    "/rules/:id",
+    pricingController.updateRule
+);
+
+router.delete(
+    "/rules/:id",
+    pricingController.deleteRule
+);
+
+router.get(
+    "/exchange-rate",
+    pricingController.getExchangeRate
+);
+
+router.put(
+    "/exchange-rate",
+    pricingController.updateExchangeRate
+);
+
+module.exports = router;
