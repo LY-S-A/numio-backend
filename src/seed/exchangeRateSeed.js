f067c4d859d37cec1941e1f0838865548e51ad1b
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-01 15:20:59 +0100

    Create exchangeRateSeed.js

---
+++ src/seed/exchangeRateSeed.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const ExchangeRate = require("../models/ExchangeRate");
+
+const exchangeRates = [
+    {
+        baseCurrency: "USD",
+        targetCurrency: "NGN",
+        rate: 1700,
+        provider: "SYSTEM",
+        enabled: true,
+    },
+];
+
+const seedExchangeRateIfEmpty = async () => {
+    try {
+
+        if (mongoose.connection.readyState === 0) {
+            await mongoose.connect(process.env.MONGODB_URI);
+        }
+
+        console.log("🔥 Checking Exchange Rates...");
+
+        const count = await ExchangeRate.countDocuments();
+
+        if (count > 0) {
+            console.log("⚡ Exchange rate already exists — skipping seed");
+            return;
+        }
+
+        await ExchangeRate.insertMany(exchangeRates);
+
+        console.log("✅ Exchange rate seeded successfully");
+
+    } catch (err) {
+
+        console.error("❌ Exchange Rate Seed Error:", err.message);
+
+    }
+};
+
+module.exports = seedExchangeRateIfEmpty;
