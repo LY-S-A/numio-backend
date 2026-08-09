1ca26758656fec339ec1a8e686d60571509f6882
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-09 09:43:21 +0100

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,29 +1,59 @@
-const sgMail = require("@sendgrid/mail");
+// const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const sendEmail = async ({
+//   to,
+//   subject,
+//   html,
+// }) => {
+//   try {
+//     await sgMail.send({
+//       to,
+//       from: process.env.EMAIL_FROM,
+//       subject,
+//       html,
+//     });
+
+//     return true;
+//   } catch (error) {
+//     console.error(
+//       "SendGrid Error:",
+//       error.response?.body || error.message
+//     );
+
+//     throw new Error("Failed to send email.");
+//   }
+// };
+
+// module.exports = sendEmail;
+
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({
-  to,
-  subject,
-  html,
+    to,
+    subject,
+    html,
 }) => {
-  try {
-    await sgMail.send({
-      to,
-      from: process.env.EMAIL_FROM,
-      subject,
-      html,
-    });
+    try {
+        await resend.emails.send({
+            from: process.env.EMAIL_FROM,
+            to,
+            subject,
+            html,
+        });
 
-    return true;
-  } catch (error) {
-    console.error(
-      "SendGrid Error:",
-      error.response?.body || error.message
-    );
+        return true;
+    } catch (error) {
+        console.error(
+            "Resend Error:",
+            error?.message || error
+        );
 
-    throw new Error("Failed to send email.");
-  }
+        throw new Error("Failed to send email.");
+    }
 };
 
 module.exports = sendEmail;
