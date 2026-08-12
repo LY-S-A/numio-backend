8166dc3bc0bf3a2413c025ca77cc0702b6817bb4
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-12 08:18:39 +0100

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,17 +1,40 @@
 const express = require("express");
+
 const router = express.Router();
 
 const {
+    adminLogin,
     getDashboardStats,
 } = require("../controllers/adminController");
 
-const { protect, adminOnly } = require("../middleware/adminAuthMiddleware");
+const {
+    protect,
+    adminOnly,
+} = require("../middleware/adminAuthMiddleware");
 
+
+/*
+========================================
+ADMIN LOGIN
+========================================
+*/
+router.post(
+    "/login",
+    adminLogin
+);
+
+
+/*
+========================================
+ADMIN DASHBOARD STATS
+========================================
+*/
 router.get(
     "/dashboard/stats",
     protect,
     adminOnly,
     getDashboardStats
 );
+
 
 module.exports = router;
