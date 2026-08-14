c90040a104779f498c7eff5c5019bf23c077b56a
lysa.waitlist <Lysauniverse30@gmail.com>
2026-08-14 10:56:14 +0100

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,76 +1,3 @@
-const express = require("express");
-
-const router = express.Router();
-
-const {
-    adminLogin,
-    getDashboardStats,
-    getUserCount,
-    sendMailToUsers,
-} = require("../controllers/adminController");
-
-const {
-    protect,
-    adminOnly,
-} = require("../middleware/adminAuthMiddleware");
-
-
-/*
-========================================
-ADMIN LOGIN
-========================================
-*/
-
-router.post(
-    "/login",
-    adminLogin
-);
-
-
-/*
-========================================
-DASHBOARD STATS
-========================================
-*/
-
-router.get(
-    "/dashboard/stats",
-    protect,
-    adminOnly,
-    getDashboardStats
-);
-
-
-/*
-========================================
-USER COUNT
-========================================
-*/
-
-router.get(
-    "/users/count",
-    protect,
-    adminOnly,
-    getUserCount
-);
-
-
-/*
-========================================
-SEND EMAIL TO ALL USERS
-========================================
-*/
-
-router.post(
-    "/users/send-mail",
-    protect,
-    adminOnly,
-    sendMailToUsers
-);
-
-
-module.exports = router;
-
 // const express = require("express");
 
 // const router = express.Router();
@@ -79,10 +6,6 @@ module.exports = router;
 //     adminLogin,
 //     getDashboardStats,
 //     getUserCount,
-//     getUserStats,
-//     getUsers,
-//     toggleUserBan,
-//     deleteUser,
 //     sendMailToUsers,
 // } = require("../controllers/adminController");
 
@@ -134,62 +57,6 @@ module.exports = router;
 
 // /*
 // ========================================
-// USER STATS
-// ========================================
-// */
-
-// router.get(
-//     "/users/stats",
-//     protect,
-//     adminOnly,
-//     getUserStats
-// );
-
-
-// /*
-// ========================================
-// GET USERS
-// ========================================
-// */
-
-// router.get(
-//     "/users",
-//     protect,
-//     adminOnly,
-//     getUsers
-// );
-
-
-// /*
-// ========================================
-// BAN / UNBAN USER
-// ========================================
-// */
-
-// router.patch(
-//     "/users/:userId/toggle-ban",
-//     protect,
-//     adminOnly,
-//     toggleUserBan
-// );
-
-
-// /*
-// ========================================
-// DELETE USER
-// ========================================
-// */
-
-// router.delete(
-//     "/users/:userId",
-//     protect,
-//     adminOnly,
-//     deleteUser
-// );
-
-
-// /* 
-// ========================================
 // SEND EMAIL TO ALL USERS
 // ========================================
 // */
@@ -203,3 +70,136 @@ module.exports = router;
 
 
 // module.exports = router;
+
+const express = require("express");
+
+const router = express.Router();
+
+const {
+    adminLogin,
+    getDashboardStats,
+    getUserCount,
+    getUserStats,
+    getUsers,
+    toggleUserBan,
+    deleteUser,
+    sendMailToUsers,
+} = require("../controllers/adminController");
+
+const {
+    protect,
+    adminOnly,
+} = require("../middleware/adminAuthMiddleware");
+
+
+/*
+========================================
+ADMIN LOGIN
+========================================
+*/
+
+router.post(
+    "/login",
+    adminLogin
+);
+
+
+/*
+========================================
+DASHBOARD STATS
+========================================
+*/
+
+router.get(
+    "/dashboard/stats",
+    protect,
+    adminOnly,
+    getDashboardStats
+);
+
+
+/*
+========================================
+USER COUNT
+========================================
+*/
+
+router.get(
+    "/users/count",
+    protect,
+    adminOnly,
+    getUserCount
+);
+
+
+/*
+========================================
+USER STATS
+========================================
+*/
+
+router.get(
+    "/users/stats",
+    protect,
+    adminOnly,
+    getUserStats
+);
+
+
+/*
+========================================
+GET USERS
+========================================
+*/
+
+router.get(
+    "/users",
+    protect,
+    adminOnly,
+    getUsers
+);
+
+
+/*
+========================================
+BAN / UNBAN USER
+========================================
+*/
+
+router.patch(
+    "/users/:userId/toggle-ban",
+    protect,
+    adminOnly,
+    toggleUserBan
+);
+
+
+/*
+========================================
+DELETE USER
+========================================
+*/
+
+router.delete(
+    "/users/:userId",
+    protect,
+    adminOnly,
+    deleteUser
+);
+
+
+/* 
+========================================
+SEND EMAIL TO ALL USERS
+========================================
+*/
+
+router.post(
+    "/users/send-mail",
+    protect,
+    adminOnly,
+    sendMailToUsers
+);
+
+
+module.exports = router;
